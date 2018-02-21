24cc68c2288a6dcf34c8830b0e0c6058577a7e68
kejace <kejace@gmail.com>
2018-02-21 14:27:02 -0500

    Works on my nix

--- Setup.hs
+++ Setup.hs
@@ -26,6 +26,7 @@ runInRepo v prog args envM = rawSystemIOWithEnv v
 
 autogen :: Args -> ConfigFlags -> IO HookedBuildInfo
 autogen _ flags = do
+    maybeExit $ runInRepo v "sh" ["libtoolize"] Nothing
     maybeExit $ runInRepo v "sh" ["./autogen.sh"] Nothing
     return emptyHookedBuildInfo
   where
